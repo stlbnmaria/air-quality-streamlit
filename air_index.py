a2432b692d1768e61c79dbc0f85d7236e8bed55b
Maria Stoelben <mariastoelben@MacBook-Pro-von-Maria.local>
2023-09-15 12:34:13 +0200

    added simple map of measuring locations

--- air_index.py
+++ air_index.py
@@ -10,16 +10,20 @@ DATA_YEARLY = 'data/aqi_yearly_1980_to_2021.csv'
 def load_data():
     data_year = pd.read_csv(DATA_YEARLY)
     data_day = pd.read_csv(DATA_DAILY)
+    data_year.rename(columns={"Latitude": "lat", "Longitude": "lon"}, inplace=True)
     return data_year, data_day
 
 st.title('Air Quality Index (1980-2021)')
-
-
-data_load_state = st.text('Loading data...')
 data_year, data_day = load_data()
-data_load_state.text("Done! (using st.cache_data)")
 
-option = st.selectbox('Show raw data', ('None', 'Daily', 'Yearly'))
+# map of measuring locations for the air quality index
+st.subheader('Locations of Measuring Stations')
+filtered_data = data_year[data_year['Year'] == max(data_year['Year'])]
+st.map(filtered_data)
+
+# option to show and inspect raw data (for the daily only the first 10k rows are displayed)
+st.subheader('Raw Data')
+option = st.selectbox('Which dataset do you want to inspect?', ('None', 'Daily', 'Yearly'))
 if option == 'Daily':
     st.dataframe(data_day.iloc[:10000,:])
 if option == 'Yearly':
